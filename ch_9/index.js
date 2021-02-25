fed14a5a125a6131727ae3194a40288bbfc1fa44
hassancode <syedhassanali85@gmail.com>
2021-02-24 20:32:59 -0500

    finished

---
+++ ch_9/index.js
@@ -0,0 +1,5 @@
+var firstName = prompt("Enter First Name");
+var lastName = prompt("Enter Last Name");
+var age = prompt("Enter Age");
+
+console.log(`Hello ${firstName} ${lastName}, You are ${age} years old`)
